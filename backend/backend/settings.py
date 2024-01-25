454e61fa80301288c68a01bbbce67b9fb35f6184
horisanLog <ttie7374@gmail.com>
2024-01-26 01:43:08 +0900

    mysql接続修正

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
+        'USER': 'root',
         'PASSWORD': 'password',
         'HOST': 'db',
         'PORT': 3306,
